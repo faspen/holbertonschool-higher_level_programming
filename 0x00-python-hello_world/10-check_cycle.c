57a46ca5d3976507bc2021939835cb2f646a442a
faspen <aspensonf@gmail.com>
2020-12-07 19:43:58 +0000

    added comments

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -16,10 +16,10 @@ int check_cycle(listint_t *list)
 
 	while (test1 && test1->next)
 	{
-		test1 = test1->next->next;
-		test2 = test2->next;
+		test1 = test1->next->next; /* iterate by 2 */
+		test2 = test2->next; /* by 1 */
 
-		if (test1 == test2)
+		if (test1 == test2) /* if they are the same, return 1 */
 			return (1);
 	}
 	return (0);
