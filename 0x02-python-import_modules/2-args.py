b99dd5d077337bfc1855bfd75d254b5b29746a4f
faspen <aspensonf@gmail.com>
2020-12-10 17:00:48 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+	import sys
+	arglen = len(sys.argv)
+
+	if (arglen == 1):
+		print("0 arguments.")
+	elif (arglen == 2):
+		print("1 argument:\n1: {}".format(sys.argv[1]))
+	else:
+		print("{} arguments:".format(arglen - 1))
+		for j in range(1, arglen):
+			print("{}: {}".format(j, sys.argv[j]))
