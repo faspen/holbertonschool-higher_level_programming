d12658a8ddee87dc23e93f2e760fa858ed1bd6c4
faspen <aspensonf@gmail.com>
2020-12-10 17:31:17 +0000

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+	import sys
+	arglen = len(sys.argv)
+	res = 0
+
+	if arglen == 1:
+		print("0")
+	elif arglen == 2:
+		print("{}".format(int(sys.argv[1])))
+	else:
+		for i in range(1, arglen):
+			res += int(sys.argv[i])
+		print("{}".format(res))
