5fb6529a55350eb3a96f85672d7703b2687c0ba6
faspen <aspensonf@gmail.com>
2020-12-15 21:19:00 +0000

    try range

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     temp = my_list.copy()
-    for i in my_list:
+    for i in range(len(my_list)):
         if (temp[i] % 2 == 0):
             temp[i] = True
         else:
