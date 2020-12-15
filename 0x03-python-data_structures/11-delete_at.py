b57a01f701e90628e45fbfac0c063f457ca7ece9
faspen <aspensonf@gmail.com>
2020-12-15 22:29:51 +0000

    one check missing grrrrr

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if (idx < 0 or idx >= len(my_list)):
+    if (idx >= 0 or idx < len(my_list)):
+        my_list.remove(my_list[idx])
         return my_list
-    for i in range(len(my_list)):
-        if (i == idx + 1):
-            my_list.remove(i)
-    return my_list
+    else:
+        return my_list
