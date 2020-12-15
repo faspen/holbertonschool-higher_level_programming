ac3f38b58765a4838a44e45414a82c8e7b48ce64
faspen <aspensonf@gmail.com>
2020-12-15 21:18:06 +0000

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    temp = my_list.copy()
+    for i in my_list:
+        if (temp[i] % 2 == 0):
+            temp[i] = True
+        else:
+            temp[i] = False
+    return temp
