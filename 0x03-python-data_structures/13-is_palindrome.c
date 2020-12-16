63fd4af8d5219dde1616bb4d26a0f7b1aaa78b79
faspen <aspensonf@gmail.com>
2020-12-16 03:25:37 +0000

    linked lists >:(

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* is_palindrome - check if list is palindrome
+* @head: top node
+* Return: 1 if true, 0 if false
+*/
+int is_palindrome(listint_t **head)
+{
+	listint_t *node, *node2;
+
+	if (!*head)
+		return (1);
+}
