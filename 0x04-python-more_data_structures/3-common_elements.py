107e86e320cfa8c01eaf71f163997d4aff8aa03d
faspen <aspensonf@gmail.com>
2020-12-17 16:52:09 +0000

    correct

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,5 +2,5 @@
 def common_elements(set_1, set_2):
     a_set = set(set_1)
     b_set = set(set_2)
-    if (set_1 & set_2):
-        return (set_1 & set_2)
+    if (a_set & b_set):
+        return (a_set & b_set)
