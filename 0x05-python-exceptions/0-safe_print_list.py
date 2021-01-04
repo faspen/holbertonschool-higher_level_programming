d22b045802b8b8d50048104c85db94b71b1930b6
faspen <aspensonf@gmail.com>
2021-01-04 15:19:13 +0000

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    num = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+            num += 1
+        except IndexError:
+            print("")
+            return num
+    print("")
+    return num
