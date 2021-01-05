326198f239c1638a5fa629d899626ddf4f1595fa
faspen <aspensonf@gmail.com>
2021-01-05 19:38:27 +0000

    added documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 class Square:
+
+    """ Square class """
+
     def __init__(self, __size):
+        """constructor for square class
+        Args:
+            size (int): size of square
+        """
         self.__size = __size
