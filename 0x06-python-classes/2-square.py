5fd473620eb9c479a1a1f57bcf53a83678e43605
faspen <aspensonf@gmail.com>
2021-01-05 23:37:50 +0000

    mispelled an LMAO

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,7 +13,7 @@ class Square:
             size (int): size of square
         """
         if type(size) is not int:
-            raise TypeError("size must be and integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
