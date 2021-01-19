88df04bb858de7b0a8c621e01dd25a62d8e2b128
faspen <aspensonf@gmail.com>
2021-01-19 16:14:29 +0000

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module with is_kind_of_class function """
+
+
+def is_kind_of_class(obj, a_class):
+    """ 
+    Return True if obj comes from
+    a_class
+    """
+    return True if isinstance(obj, a_class) else False
