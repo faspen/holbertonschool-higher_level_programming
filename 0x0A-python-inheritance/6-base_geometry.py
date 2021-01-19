172b249d7e791cc4c904c5cf873fd6ec6628cee1
faspen <aspensonf@gmail.com>
2021-01-19 19:33:41 +0000

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module containing bg class with area method """
+
+
+class BaseGeometry:
+
+    """ Geometry class
+    """
+
+    def area(self):
+        """ incomplete area function
+        """
+        raise Exception('area() is not implemented')
