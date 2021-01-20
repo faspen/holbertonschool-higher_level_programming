52d16e5f74730d03d57821c7787f2781d99a11d9
faspen <aspensonf@gmail.com>
2021-01-20 16:39:26 +0000

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module that loads, adds, and saves """
+import os
+from sys import argv
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+arg_list = []
+
+if os.path.exists("add_item.json"):
+    arg_list = load_from_json_file("add_item.json")
+save_to_json_file(arg_list + argv[1:], "add_item.json")
