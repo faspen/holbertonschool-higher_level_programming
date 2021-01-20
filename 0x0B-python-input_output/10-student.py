9ae7f1056b19527dca7875a585c74b2dfacf1f31
faspen <aspensonf@gmail.com>
2021-01-20 17:11:57 +0000

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Module with student class """
+
+
+class Student:
+
+    """ Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialization for student class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieve dict of Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for names in attrs:
+                for i in self.__dict__:
+                    if i == names:
+                        new_dict[i] = self.__dict__[i]
+            return new_dict
