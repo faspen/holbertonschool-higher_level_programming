e91ea764a7de1f8cb43ed8b143af2f6319e67d0a
faspen <aspensonf@gmail.com>
2021-01-20 19:49:00 +0000

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module containing pascal_triangle function """
+import math
+
+
+def combination(n, r):
+    """ Helper function
+    """
+    return int((math.factorial(n)) / ((math.factorial(r)) * math.factorial(n - r)))
+
+
+def pascal_triangle(n):
+    """ Print pascal triangle
+    """
+    if n <= 0:
+        return []
+
+    result = []
+    for count in range(n):
+        tmp = []
+        for element in range(count + 1):
+            tmp.append(combination(count, element))
+        result.append(tmp)
+    return result
