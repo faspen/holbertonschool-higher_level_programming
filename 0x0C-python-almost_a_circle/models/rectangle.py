80cd36631afc29369c6d31a1710c4da84d0f34e6
faspen <aspensonf@gmail.com>
2021-01-22 16:34:27 +0000

    add args and kwargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,3 +88,20 @@ class Rectangle(Base):
         """ Return printable representation """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """Assign arguments"""
+        for i in range(len(args)):
+            if i == 0:
+                super().__init__(args[0])
+            if i == 1:
+                self.width = args[1]
+            if i == 2:
+                self.height = args[2]
+            if i == 3:
+                self.x = args[3]
+            if i == 4:
+                self.y = args[4]
+
+        for k, v in kwargs.items():
+            setattr(self, k, v)
