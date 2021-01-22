240a6a1002c58b0b99f65553ee2f8929df254ea5
faspen <aspensonf@gmail.com>
2021-01-22 17:11:40 +0000

    updated square and rectangle, adding all mains

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,8 @@ class Rectangle(Base):
 
         for k, v in kwargs.items():
             setattr(self, k, v)
+
+    def to_dictionary(self):
+        """ Return dict """
+        return {"id": self.id, "width": self.width, "height": self.height,
+                "x": self.x, "y": self.y}
