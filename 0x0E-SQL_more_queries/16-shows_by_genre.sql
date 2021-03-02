f9bb83477ab42873b140edad83555e1bad441474
faspen <2335@holbertonschool.com>
2021-03-02 22:29:59 +0000

    16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- List all shows and genres linked to each
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_genres.name
