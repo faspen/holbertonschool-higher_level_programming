713fa69fe7b34c446e916f54ca39fc259a00fa58
faspen <2335@holbertonschool.com>
2021-03-02 19:35:16 +0000

    12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- List shows with no genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+	FROM tv_show_genres
+	RIGHT OUTER JOIN tv_shows
+		ON tv_shows.id = tv_show_genres.show_id
+		WHERE show_id IS NULL
+		ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
