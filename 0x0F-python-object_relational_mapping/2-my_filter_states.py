f48b56ea3924380afcf5e631434e00c0e4e623cb
faspen <2335@holbertonschool.com>
2021-03-25 16:00:24 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Takes an argument and displays all states
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+        id ASC".format(argv[4]))
+    output = cursor.fetchall()
+    for item in output:
+        print(item)
+    cursor.close()
+    db.close()
