32c519e56cd4fc0f55538f2707b8ff4d48c285d3
faspen <2335@holbertonschool.com>
2021-04-09 17:31:58 +0000

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,17 +2,17 @@
 """Task 3 module"""
 
 
-from sys import argv
+import sys
 import urllib.request
 import urllib.error
 
 
 if __name__ == "__main__":
 
-    req = urllib.request.Request(argv[1])
+    req = urllib.request.Request(sys.argv[1])
     try:
         with urllib.request.urlopen(req) as response:
-	    resp = response.read()
+            resp = response.read()
             print(resp.decode('utf-8'))
     except urllib.error.HTTPError as e:
         print('Error code: {}'.format(e.code))
