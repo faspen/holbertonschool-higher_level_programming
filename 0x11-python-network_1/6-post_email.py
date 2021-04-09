125cd830fcad096a699a2dfbc21a7e0d235b95b0
faspen <2335@holbertonschool.com>
2021-04-09 18:03:30 +0000

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
-    r = requests.post(url, value)
+    r = requests.post(url, data=value)
     print(r.text.decode('utf-8'))
