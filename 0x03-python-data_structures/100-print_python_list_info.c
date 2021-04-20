6e45d7d2e5c1a8ef0a90ff06b9936bbb9442c828
faspen <2335@holbertonschool.com>
2021-04-20 14:51:18 +0000

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,16 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+* print_python_list_info - print info
+* @p: pointer
+*/
+void print_python_list_info(PyObject *p)
+{
+	unsigned int index;
+
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (index = 0; index < PyList_Size(p); index++)
+		printf("Element %d: %s\n", index, Py_TYPE(PyList_GetItem(p, index))->tp_name);
+}
