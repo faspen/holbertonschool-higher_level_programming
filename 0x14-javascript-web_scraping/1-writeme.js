258250414ab6b7500e7ea3245d04627e52c4af0b
faspen <2335@holbertonschool.com>
2021-05-04 14:51:31 +0000

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* Write to file */
+
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+});
