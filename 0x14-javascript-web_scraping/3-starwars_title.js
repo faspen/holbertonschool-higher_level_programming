5ad5bf47cf0c2bfc1176b32ec7a8eb15eb970d63
faspen <2335@holbertonschool.com>
2021-05-04 18:12:24 +0000

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,5 +6,5 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
 const movie = process.argv[2];
 request(url + movie, function (err, response, body) {
-    console.log(err || JSON.parse(body).title);
+  console.log(err || JSON.parse(body).title);
 });
