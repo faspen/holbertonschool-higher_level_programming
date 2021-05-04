d35ae0517e771765528bf813ee1770c0952b6563
faspen <2335@holbertonschool.com>
2021-05-04 18:05:54 +0000

    no const

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,13 +3,13 @@
 
 const request = require('request');
 
-const url = 'https://swapi-api.hbtn.io/api/films/';
-const movie = process.argv[2];
+let url = 'https://swapi-api.hbtn.io/api/films/';
+let movie = process.argv[2];
 request(url + movie, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    const tmp = JSON.parse(body);
+    let tmp = JSON.parse(body);
     console.log(tmp.title);
   }
 });
